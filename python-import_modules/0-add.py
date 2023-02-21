20ccdf7d6b96fc2d84af1a9887989682513ec4c2
winnyineza <w.ineza@alustudent.com>
2023-02-21 13:51:16 -0800

    Commit all changes

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+"""Importing function from a file"""
 from add_0 import add
 
 a = 1
 b = 2
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
-if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, add(a, b)))
--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,17 @@
-#!/usr/bin/python3
-from sys import argv
-if __name__ == "__main__":
-    if len(argv) == 1:
-        print("0 arguments.")
-    elif len(argv) == 2:
-        print("1 argument:")
-        print("1: {}".format(argv[1]))
-    else:
-        print("{} arguments:".format(len(argv) - 1))
-        for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
+#!/usr/bin/env python3
+
+import sys
+
+args = sys.argv[1:]
+num_args = len(args)
+
+if num_args == 0:
+    print("0 arguments.")
+elif num_args == 1:
+    print("1 argument:")
+    print("1: {}".format(args[0]))
+else:
+    print("{} arguments:".format(num_args))
+    for i, arg in enumerate(args):
+        print("{}: {}".format(i+1, arg))
+
