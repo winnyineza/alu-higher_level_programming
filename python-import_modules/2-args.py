f01911fb5f47f7e65dab3dcb69a86531e559c527
Winny Didine Ineza <116871728+winnyineza@users.noreply.github.com>
2023-02-21 23:45:23 +0200

    Task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 from sys import argv
-if __name__ == "__main__":
+
+
+def principal():
+    print('{} argument'.format(len(argv) - 1), end='')
     if len(argv) == 1:
-        print("0 arguments.")
+        print('s.')
     elif len(argv) == 2:
-        print("1 argument:")
-        print("1: {}".format(argv[1]))
+        print(':')
     else:
-        print("{} arguments:".format(len(argv) - 1))
-        for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
+        print('s:')
+    for i in range(1, len(argv)):
+        print('{}: {}'.format(i, argv[i]))
+
+
+if __name__ == "__main__":
+    principal()
