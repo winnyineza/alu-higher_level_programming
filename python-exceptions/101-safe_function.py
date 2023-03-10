94526df89e6c0479e0738ec60d1c1ff95fb5e3c0
Winny Didine Ineza <116871728+winnyineza@users.noreply.github.com>
2023-03-10 22:31:40 +0200

    Task 101

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-import sys
-
-
 def safe_function(fct, *args):
     try:
-        result = fct(*args)
-    except Exception as e:
-        print("Exception: {}".format(e), file=sys.stderr)
+        r = fct(*args)
+        return r
+    except Exception as error:
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
         return None
-    else:
-        return 
