20a2118e89e9a88d7e5f11c63d5bdff42b8bc2a5
Winny Didine Ineza <116871728+winnyineza@users.noreply.github.com>
2023-03-24 23:13:12 +0200

    Task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-"""adds all arguments to a Python list, and then save them to a file"""
+"""A script that adds all arguments to a python list"""
 
-import json
+
 import sys
-from os import path
-from typing import List
-
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-
-def add_item(filename: str, args: List[str]):
-    """Adds all arguments to a Python list and saves them to a file"""
-    new_list = []
-    if path.exists(filename):
-        new_list = load_from_json_file(filename)
-    new_list += args
-    save_to_json_file(new_list, filename)
-
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    add_item("add_item.json", args)
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file') \
+        .load_from_json_file
+
+    try:
+        loadFile = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        loadFile = []
+
+    argc = len(sys.argv)
+    for idx in range(1, argc):
+        loadFile.append(sys.argv[idx])
+    save_to_json_file(loadFile, "add_item.json")
