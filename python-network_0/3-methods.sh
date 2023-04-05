80200ca0acbbabeafbd39aceb3f0dcdaa7aa8582
Winny Didine Ineza <116871728+winnyineza@users.noreply.github.com>
2023-04-05 12:47:59 +0200

    Delete python-network_0 directory

--- python-network_0/3-methods.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#  a Bash script that takes in a URL and displays all HTTP methods the server will accept
-curl -si "$1" | awk -F ": " '/Allow/ {print $2}'
--- python-network_0/README.md
+++
@@ -1,1 +0,0 @@
-Pythong - Networking - 0
