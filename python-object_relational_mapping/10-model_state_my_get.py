c38eed427bf541672ac535b56b9b4d5328d43ec9
Winny Didine Ineza <116871728+winnyineza@users.noreply.github.com>
2023-04-26 09:13:04 +0200

    Task 10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""a script that prints the State object with
+the name passed as argument"""
+
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).filter_by(name=argv[4]):
+        print("{}".format(instance.id))
+        break
+    else:
+        print("Not found")
+    session.close()
