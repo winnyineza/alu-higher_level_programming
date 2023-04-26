a91c8b438d94674316a04d7490bc248f0b7f0719
winnyineza <w.ineza@alustudent.com>
2023-04-26 00:46:03 -0700

    Task 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-"""List all cities from the database"""
+"""
+Script that lists all cities from the database hbtn_0e_4_usa
+"""
+import MySQLdb
+from sys import argv
 
-
-if __name__ == "__main__":
-    from sys import argv
-    import MySQLdb
-    # connect database to python file
-    db = MySQLdb.connect(host="localhost", user=argv[1],
+# The code should not be executed when imported
+if __name__ == '__main__':
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
-    # create cursor to exec queries using SQL
+
     cur = db.cursor()
-    cur.execute("""SELECT c.id, c.name, s.name FROM cities AS c \
-                INNER JOIN states AS s ON c.state_id = s.id \
-                ORDER BY c.id ASC""")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                ORDER BY cities.id ASC")
+
     rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    for i in rows:
+        print(i)
+    # Clean up process
     cur.close()
     db.close()
