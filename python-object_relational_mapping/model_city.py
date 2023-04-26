23f6dc15479001743fd0eaaf8e3582980ef65feb
Winny Didine Ineza <116871728+winnyineza@users.noreply.github.com>
2023-04-26 09:03:38 +0200

    ORM

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a python file that contains the class definition of a City"""
+
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """
+    defines the class City that links to MySQL table 'cities'
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
