b2542c3b22b640faec28c860497d7ff0b20212bd
winnyineza <w.ineza@alustudent.com>
2023-04-26 00:35:17 -0700

    Task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-"""a script that takes in an argument and displays all values in the states"""
+"""
+Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument
+"""
+import MySQLdb
+from sys import argv
 
+# The code should not be executed when imported
+if __name__ == '__main__':
 
-if __name__ == "__main__":
-    from sys import argv
-    import MySQLdb
-    db = MySQLdb.connect(host="localhost", user=argv[1],
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
+
+    # It gives us the ability to have multiple seperate working environments
+    # through the same connection to the database.
     cur = db.cursor()
-    cur.execute("""SELECT id, name FROM states WHERE name LIKE BINARY '{}' \
-                ORDER BY states.id ASC""".format(argv[4]))
+    nmeSr = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4])
+    cur.execute(nmeSr)
+
     rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    for i in rows:
+        print(i)
+    # Clean up process
     cur.close()
     db.close()
