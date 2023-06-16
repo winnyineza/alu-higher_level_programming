653cc4ac48cd75885bf159088a131342ecc4c7e7
winnyineza <w.ineza@alustudent.com>
2023-06-16 05:25:48 -0700

    Task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,20 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-function readFile (filePath) {
-  fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-      console.error(`An error occurred while reading the file: ${err}`);
-    } else {
-      console.log(data);
-    }
-  });
-}
-
-// Check if the file path is provided as an argument
-if (process.argv.length > 2) {
-  const filePath = process.argv[2];
-  readFile(filePath);
-} else {
-  console.log('Please provide the file path as an argument.');
-}
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
