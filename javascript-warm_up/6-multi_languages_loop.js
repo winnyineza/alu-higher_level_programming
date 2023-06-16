546873b8da62e35a344d17f4dc20ce74c2fa2284
winnyineza <w.ineza@alustudent.com>
2023-06-16 04:07:23 -0700

    Task complete

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (const language of languages) {
-  console.log(language);
+const strs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const str of strs) {
+  console.log(str);
 }
