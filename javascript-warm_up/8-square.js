f94cc0f3b0a1d8d0b17e7241ab66a68c885696ed
winnyineza <w.ineza@alustudent.com>
2023-06-16 04:10:31 -0700

    Task 8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,18 +1,12 @@
 #!/usr/bin/node
-const size = parseInt(process.argv[2]);
+const number = parseInt(process.argv[2]);
 
-if (Number.isNaN(size)) {
-  console.log('Missing size');
-} else {
-  if (size > 0) {
-    for (let i = 0; i < size; i++) {
-      let line = '';
-      for (let j = 0; j < size; j++) {
-        line += 'X';
-      }
-      console.log(line);
-    }
-  } else {
-    console.log('Invalid size');
+if (number) {
+  for (let i = 0; i < number; i++) {
+    console.log('X'.repeat(number));
   }
+} else if (number < 0) {
+  console.log();
+} else {
+  console.log('Missing size');
 }
